e3f2f9f8e9cd0f38cc23da5abacf5e98a8fd4b36
Adam Hoyle <atomoil@gmail.com>
2015-03-09 08:59:11 +0000

    basic html / canvas / js / paperjs setup

---
+++ deploy/js/londonsquared.js
@@ -0,0 +1,9 @@
+
+
+function londonsquared_init()
+{
+	// draw the grey rectangle at the bottom
+	var re2 = new paper.Path.Rectangle(new paper.Point(0, 0), new paper.Point(100, 100));
+	re2.fillColor = '#ffEDED';
+	
+}
