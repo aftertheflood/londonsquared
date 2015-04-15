e1921459261b59bc71572e3ec6970f29aef7c156
Adam Hoyle <atomoil@gmail.com>
2015-04-15 16:25:00 +0100

    update the window resizing code

--- deploy/js/window-size.js
+++ deploy/js/window-size.js
@@ -1,14 +1,22 @@
 (function() {
-    
-
     // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
 
+	var max_width = 1000; // no bigger than 1000px wide
+	
+	// work out ratio based on original design.
+	var o_w = 872;
+	var o_h = 761;
+	var ratio = o_h / o_w;		
+
     function resizeCanvas() {
-            var canvas = document.getElementById('londonsquared_canvas');
-            console.log("look ma no hands")
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
+    	var canvas = document.getElementById('londonsquared_canvas');
+		if (canvas){
+			var wid = Math.min( window.innerWidth, max_width );
+			
+			canvas.width = wid;
+			canvas.height = wid * ratio;
+		}
     }
     resizeCanvas();
 })();
