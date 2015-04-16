39001ccf27cfe4003498920c2c7df8d1741f7a2f
Adam Hoyle <atomoil@gmail.com>
2015-04-16 23:37:28 +0100

    reize canvas dynamically (and also do it on first load) - look ma, no jquery

--- deploy/js/window-size.js
+++ deploy/js/window-size.js
@@ -1,22 +1,26 @@
 (function() {
     // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
+    window.addEventListener('resize', resizeCanvas, false)
 
-	var max_width = 1000; // no bigger than 1000px wide
+	var max_width = 1000 // no bigger than 1000px wide
 	
 	// work out ratio based on original design.
-	var o_w = 872;
-	var o_h = 761;
-	var ratio = o_h / o_w;		
+	var o_w = 872
+	var o_h = 761
+	var ratio = o_h / o_w	
 
     function resizeCanvas() {
-    	var canvas = document.getElementById('londonsquared_canvas');
+    	var canvas = document.getElementById('londonsquared_canvas')
 		if (canvas){
-			var wid = Math.min( window.innerWidth, max_width );
+			var wid = Math.min( window.innerWidth, max_width )
+			var hei = wid * ratio
 			
-			canvas.width = wid;
-			canvas.height = wid * ratio;
+			canvas.width = wid
+			canvas.height = hei
+			canvas.style.width = wid+"px"
+			canvas.style.height = hei+"px"
 		}
     }
     resizeCanvas();
+    window.addEventListener('load',resizeCanvas,false) // resizeCanvas as soon as page loads
 })();
